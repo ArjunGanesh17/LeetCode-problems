8732c89244c403dddc5b5ef1d2dc0b52159d8efb
Ganesh Reddy <150496081+ArjunGanesh17@users.noreply.github.com>
2024-05-31 10:39:32 +0530

    NumberofSteps.java

--- NumberofSteps.java
+++ NumberofSteps.java
@@ -1,3 +1,16 @@
+/*
+Leetcode 1342
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+number of steps to reach 0
+eg n=14
+step 1 n is even n/2=7 
+step 2 n is odd n=n-1 6
+step 3 even 6/2 = 3
+step 4 odd 3-1=2
+step 5 even 2/2=1
+step 6 odd 1-1=0
+return steps
+ */
 public class NumberofSteps {
     public static void main(String[] args) {
         System.out.println(steps(14));
